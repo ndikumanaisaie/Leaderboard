fd2d83ab0ab5982931b4c839a7e07bbe48db214b
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-06-17 08:52:13 +0100

    Add async and await to getscore function

--- src/utility_functions.js
+++ src/utility_functions.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-cycle */
 import displayScore from './dom_functions.js';
 
-const requestedURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/';
+const requestedURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/TlqN76u4EY0I0EumuJl5/scores/';
 export const sendData = async (name, score) => {
   await fetch(requestedURL, {
     method: 'POST',
@@ -17,8 +17,8 @@ export const sendData = async (name, score) => {
     .then((json) => json.result);
 };
 
-export const getScores = () => {
-  fetch(requestedURL, {
+export const getScores = async () => {
+  await fetch(requestedURL, {
     method: 'GET',
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
